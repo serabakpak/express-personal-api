762035554b1de7cbcb3bcac0c0b2cf0793abb7b4
Sera Bak <serabak15@gmail.com>
2016-09-05 23:31:27 -0700

    updates documentation endpoints

--- views/docs/index.js
+++ views/docs/index.js
@@ -1,6 +1,5 @@
 module.exports = {
     
-    whoopsIForgotToDocumentAllMyEndpoints: true, // I liked this name ;)
     message: "Welcome to Sera's personal api! Here's what you need to know!",
     documentationUrl: "https://github.com/serabakpak/express-personal-api/blob/master/README.md", 
     baseUrl: "https://fathomless-plains-84466.herokuapp.com/", 
@@ -8,6 +7,9 @@ module.exports = {
       {method: "GET", path: "/api", description: "Describes all available endpoints"},
       {method: "GET", path: "/api/profile", description: "Data about me"},
       {method: "GET", path: "/api/destinations", description: "List all travel destinations I would like to visit one day."}, 
-      {method: "POST", path: "/api/destinations", description: "E.g. Create a new travel destination."} 
+      {method: "GET", path: "/api/destinations/:id", description: "Show one travel destination."}, 
+      {method: "POST", path: "/api/destinations", description: "Create a new travel destination."},
+      {method: "PUT", path: "/api/destinations/:id", description: "Edit the description for a particular destination."} ,
+      {method: "DELETE", path: "/api/destinations/:id", description: "Delete a travel destination."}  
     ]
   }
